b893861a6c5d4f250c60131519c5ee876d78fc28
annacwu <annacwu04@gmail.com>
2024-11-04 16:25:52 -0300

    Add completed button functionality

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ interface Habit {
     name: string;
     frequency: string; // FIXME: make this into its own enum later
     completions: number;
+    completionDates: string[];
 }
 
 // get references to the DOM elements
@@ -25,9 +26,34 @@ habitForm.addEventListener('submit', (event) => {
 });
 
 function addHabit(name: string, frequency: string) {
-    const newHabit: Habit = {name, frequency, completions: 0};
+    const newHabit: Habit = {name, frequency, completions: 0, completionDates: []};
     const li = document.createElement('li'); // makes a new list element in the html
-    li.textContent = `${newHabit.name} (${newHabit.frequency})`;
+    li.textContent = `${newHabit.name} (${newHabit.frequency}) - Completed: ${newHabit.completions}`;
 
+    // mark as completed button
+    const completeButton = document.createElement('button');
+    completeButton.textContent = 'Complete'
+    completeButton.addEventListener('click', () => markAsCompleted(newHabit));
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+        li.remove();
+    });
+
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
     habitList.appendChild(li);
+}
+
+function markAsCompleted(habit: Habit) {
+    const today = new Date().toISOString().split('T')[0];
+    if (!habit.completionDates.includes(today)) {
+        habit.completionDates.push(today);
+        habit.completions++;
+        alert(`Habit ${habit.name} completed.`);
+    } else {
+        alert(`You've already completed ${habit.name} today.`);
+    }
 }
