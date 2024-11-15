0c1624e3be5e294e95b72e3f58819af12a58a696
annacwu <annacwu04@gmail.com>
2024-11-15 11:04:08 -0300

    Change frequency selection in UI

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,39 @@
 interface Habit {
     name: string;
-    frequency: string; // FIXME: make this into its own enum later
+    frequency: Frequency; // FIXME: make this into its own enum later
     completions: number;
     completionDates: string[];
 }
+
+type Frequency = "Daily" | "Weekly" | "Twice a Week" | "Custom"
+
+interface CustomFrequency {
+    days: string[]; // e.g., ['Monday', 'Wednesday']
+  }
 
 const habits: Habit[] = loadFromStorage(); // loads as json data?
 
 // get references to the DOM elements
 const habitForm = document.getElementById('habit-form') as HTMLFormElement;
 const habitInput = document.getElementById('habit-input') as HTMLInputElement;
-const frequencyInput = document.getElementById('frequency-input') as HTMLInputElement;
+const frequencyInput = document.getElementById('frequency-input') as HTMLSelectElement;
+const customFrequencyInput = document.getElementById('custom-frequency-input') as HTMLDivElement;
 const habitList = document.getElementById('habit-list') as HTMLUListElement;
 
 habits.forEach(habit => addHabit(habit)); // AFTER the DOM elements so it knows what to reference
 
+frequencyInput.addEventListener('change', () => {
+    if (frequencyInput.value === "Custom") {
+        customFrequencyInput.style.display = "block"; // Show custom frequency options
+    } else {
+        customFrequencyInput.style.display = "none"; // Hide custom frequency options
+    }
+});
+
 habitForm.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent page refresh on form submission
     const habitName = habitInput.value.trim(); // trim removes leading/trailing whitespace
-    const habitFrequency = frequencyInput.value.trim();
+    const habitFrequency = frequencyInput.value as Frequency;
 
     if (habitName.length > 0 && habitFrequency.length > 0) {
         const newHabit: Habit = {name: habitName, frequency: habitFrequency, completions: 0, completionDates: []};
